57abbb7c783fe45012bd830a66da69e455cf39ca
Eugenio Barquin <eu.barquin@me.com>
2016-12-03 23:30:05 +0100

    Ejemplos de Bindings

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { ListaContactosComponent } from './lista-contactos/lista-contactos.component'
 
+import { BindingComponent} from "./ejemplos/binding.component"
 @NgModule({
   //Usamos 'imports' para importar otros módulos
   //de los cuales dependemos.
   imports: [
-     BrowserModule
+     BrowserModule, FormsModule
   ],
   //En 'declarations' añadimos todos los componentes
   // pipes y directivas de nuestra aplicación
   declarations: [
-     AppComponent, ListaContactosComponent 
+     AppComponent, 
+     ListaContactosComponent,
+     BindingComponent 
   ],
   //En 'providers' añadimos todos los servicios de
   //nuestra aplicación
@@ -22,6 +26,7 @@ import { ListaContactosComponent } from './lista-contactos/lista-contactos.compo
   //nuestra aplicación.
   bootstrap: [
      AppComponent
+     //BindingComponent
   ]
 })
 export class AppModule { }
